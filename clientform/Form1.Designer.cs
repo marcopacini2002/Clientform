43385286a7a88c8040235a18c4b9c680990fd5b4
marcopacini2002 <marco02pacio@gmail.com>
2020-12-21 10:50:21 +0100

    commit

--- clientform/Form1.Designer.cs
+++ clientform/Form1.Designer.cs
@@ -29,13 +29,143 @@ namespace clientform
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstMessaggiServ = new System.Windows.Forms.ListBox();
+            this.btnInvia = new System.Windows.Forms.Button();
+            this.txtMessaggio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnetti = new System.Windows.Forms.Button();
+            this.txtPorta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIPServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstMessaggiServ
+            // 
+            this.lstMessaggiServ.FormattingEnabled = true;
+            this.lstMessaggiServ.Location = new System.Drawing.Point(11, 90);
+            this.lstMessaggiServ.Margin = new System.Windows.Forms.Padding(2);
+            this.lstMessaggiServ.Name = "lstMessaggiServ";
+            this.lstMessaggiServ.Size = new System.Drawing.Size(468, 277);
+            this.lstMessaggiServ.TabIndex = 17;
+            // 
+            // btnInvia
+            // 
+            this.btnInvia.Enabled = false;
+            this.btnInvia.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnInvia.Location = new System.Drawing.Point(284, 44);
+            this.btnInvia.Margin = new System.Windows.Forms.Padding(2);
+            this.btnInvia.Name = "btnInvia";
+            this.btnInvia.Size = new System.Drawing.Size(56, 26);
+            this.btnInvia.TabIndex = 16;
+            this.btnInvia.Text = "Invia";
+            this.btnInvia.UseVisualStyleBackColor = true;
+            this.btnInvia.Click += new System.EventHandler(this.btnInvia_Click);
+            // 
+            // txtMessaggio
+            // 
+            this.txtMessaggio.Enabled = false;
+            this.txtMessaggio.Location = new System.Drawing.Point(76, 49);
+            this.txtMessaggio.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMessaggio.Name = "txtMessaggio";
+            this.txtMessaggio.Size = new System.Drawing.Size(191, 20);
+            this.txtMessaggio.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 50);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Messaggio:";
+            // 
+            // btnConnetti
+            // 
+            this.btnConnetti.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConnetti.Location = new System.Drawing.Point(284, 11);
+            this.btnConnetti.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConnetti.Name = "btnConnetti";
+            this.btnConnetti.Size = new System.Drawing.Size(56, 27);
+            this.btnConnetti.TabIndex = 13;
+            this.btnConnetti.Text = "Connetti";
+            this.btnConnetti.UseVisualStyleBackColor = true;
+            this.btnConnetti.Click += new System.EventHandler(this.btnConnetti_Click);
+            // 
+            // txtPorta
+            // 
+            this.txtPorta.Location = new System.Drawing.Point(190, 15);
+            this.txtPorta.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPorta.Name = "txtPorta";
+            this.txtPorta.Size = new System.Drawing.Size(76, 20);
+            this.txtPorta.TabIndex = 12;
+            this.txtPorta.Text = "23000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Porta";
+            // 
+            // txtIPServer
+            // 
+            this.txtIPServer.Location = new System.Drawing.Point(63, 15);
+            this.txtIPServer.Margin = new System.Windows.Forms.Padding(2);
+            this.txtIPServer.Name = "txtIPServer";
+            this.txtIPServer.Size = new System.Drawing.Size(76, 20);
+            this.txtIPServer.TabIndex = 10;
+            this.txtIPServer.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "IP Server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(493, 379);
+            this.Controls.Add(this.lstMessaggiServ);
+            this.Controls.Add(this.btnInvia);
+            this.Controls.Add(this.txtMessaggio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConnetti);
+            this.Controls.Add(this.txtPorta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIPServer);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstMessaggiServ;
+        private System.Windows.Forms.Button btnInvia;
+        private System.Windows.Forms.TextBox txtMessaggio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnetti;
+        private System.Windows.Forms.TextBox txtPorta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIPServer;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- clientform/Form1.cs
+++ clientform/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,82 @@ namespace clientform
 {
     public partial class Form1 : Form
     {
+        Socket client;
         public Form1()
         {
+           
             InitializeComponent();
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        private void btnConnetti_Click(object sender, EventArgs e)
+        {
+            IPAddress ipaddr = null;
+            int nPort = 0;
+
+            if (!IPAddress.TryParse(txtIPServer.Text, out ipaddr))
+            {
+                MessageBox.Show("Ip vuoto o non valido.", "Errore");
+                
+            }
+
+            if (!int.TryParse(txtPorta.Text, out nPort))
+            {
+                MessageBox.Show("Porta vuota o non valida", "Errore");
+            }
+
+            if (nPort < 0 && nPort > 65535)
+            {
+                MessageBox.Show("La porta deve essere compresa fra 0 e 65535", "Errore");
+            }
+            try
+            {
+
+                client.Connect(ipaddr, nPort);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore di connessione", "Errore");
+                if (client != null)
+                {
+                    if (client.Connected)
+                    {
+                        client.Shutdown(SocketShutdown.Both);
+                    }
+                    client.Close();
+
+                    client.Dispose();
+                }
+            }
+            txtIPServer.Enabled = false;
+            txtPorta.Enabled = false;
+            btnConnetti.Enabled = false;
+
+            txtMessaggio.Enabled = true;
+            btnInvia.Enabled = true;
+        }
+
+        private void btnInvia_Click(object sender, EventArgs e)
+        {
+            byte[] sendBuff = new byte[512];
+            byte[] recvBuff = new byte[512];
+            int nRecvBytes = 0;
+            string recvString = "";
+            try
+            {
+                sendBuff = Encoding.ASCII.GetBytes(txtMessaggio.Text);
+                client.Send(sendBuff);
+
+                nRecvBytes = client.Receive(recvBuff);
+                recvString = Encoding.ASCII.GetString(recvBuff, 0, nRecvBytes);
+
+                lstMessaggiServ.Items.Add(recvString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Errore");
+
+            }
         }
     }
 }
